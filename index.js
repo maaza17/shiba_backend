0d40fd547877e611cc8117ca6c6c439078748ae7
haris316 <gogeto931@gmail.com>
2021-12-17 20:21:16 +0500

    Network Error Resolved

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const passport = require('passport')
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const env = require("dotenv");
+env.config();
+const users = require("./routes/api/users");
+const places = require("./routes/api/places");
+const reviews = require("./routes/api/review");
+const admin = require("./routes/admin/admin");
 
-const users = require('./routes/api/users')
-const places = require('./routes/api/places')
-const reviews = require('./routes/api/review')
-const admin = require('./routes/admin/admin')
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const app = express()
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-const db = require('./config/keys').mongoURI
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> console.log('Database connected successfully'))
-.catch((err) => console.log(err))
 
-app.use(passport.initialize())
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log(err));
 
-require('./config/passport')(passport)
+app.use(passport.initialize());
 
-app.use('/api/users', users)
-app.use('/api/places', places)
-app.use('/api/reviews', reviews)
-app.use('/admin', admin)
+require("./config/passport")(passport);
 
-const port = 9002 || process.env.PORT
-app.listen(port, () => console.log('Server is live on port ' + port + '!'))
+app.use("/api/users", users);
+app.use("/api/places", places);
+app.use("/api/reviews", reviews);
+app.use("/admin", admin);
+
+const port = 9002 || process.env.PORT;
+app.listen(port, () => console.log("Server is live on port " + port + "!"));
