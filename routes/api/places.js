0a6aab01f23bd09e42d4ca204e2102172d7a58a6
haris316 <gogeto931@gmail.com>
2021-12-25 18:45:09 +0500

    Review Changes

--- routes/api/places.js
+++ routes/api/places.js
@@ -1,69 +1,98 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const placeModel = require('../../models/Place')
+const placeModel = require("../../models/Place");
+const userModel = require("../../models/User");
 
 // Add place
-router.post('/addplace', (req, res) => {
-    
-    const newPlace = new placeModel({place_name: req.body.place_name, lng: req.body.lng, lat: req.body.lat, category: req.body.category,
-        images: req.body.images, desc: req.body.desc, website: req.body.website, createdBy: req.body.createdBy})
-    
-    newPlace.save((err, place) => {
-        if(err){
-            return res.status(400).json({
-                error: true,
-                message: err.message
-            })
+router.post("/addplace", (req, res) => {
+  userModel.findOne({ email: req.body.createdByEmail }, (error, user) => {
+    if (error) {
+      return res.status(200).json({
+        error: true,
+        message: error.message,
+      });
+    } else if (user) {
+      const newPlace = new placeModel({
+        place_name: req.body.place_name,
+        lng: req.body.lng,
+        lat: req.body.lat,
+        category: req.body.category,
+        images: req.body.images,
+        desc: req.body.desc,
+        website: req.body.website,
+        createdBy: user._id,
+      });
+      newPlace.save((err, place) => {
+        if (err) {
+          return res.status(200).json({
+            error: true,
+            message: err.message,
+          });
         } else {
-            return res.status(200).json({
-                error: false,
-                message: 'Place added! Pending approval',
-                data: place
-            })
+          return res.status(200).json({
+            error: false,
+            message: "Place added! Pending approval",
+            data: place,
+          });
         }
-    })   
-})
+      });
+    }
+  });
+});
 
 // Get all places
-router.get('/getapprovedplaces', (req, res) => {
-    placeModel.find({is_deleted: false, is_approved: true}, (err, docs) => {
-        if(err){
-            return res.status(400).json({
-                error: true,
-                message: err.message
-            })
-        } else {
-            return res.status(200).json({
-                error: false,
-                message: 'Places found!',
-                data: docs
-            })
-        }
-    })
-})
+router.get("/getapprovedplaces", (req, res) => {
+  placeModel.find({ is_deleted: false, is_approved: true }, (err, docs) => {
+    if (err) {
+      return res.status(400).json({
+        error: true,
+        message: err.message,
+      });
+    } else {
+      return res.status(200).json({
+        error: false,
+        message: "Places found!",
+        data: docs,
+      });
+    }
+  });
+});
 
 // Get nearby places within a 10km radius
-router.get('/getnearby', (req, res) => {
-    const {lng, lat} = req.body
+router.get("/getnearby", (req, res) => {
+  const { lng, lat } = req.body;
 
-    placeModel.find({is_deleted: false, is_approved: true}).and([
-        {$and:[{lat: {$gte: Number(lat)-0.09009}},{lat: {$lte: Number(lat)+0.09009}}]},
-        {$and:[{lng: {$gte: Number(lng)-0.09009}},{lng: {$lte: Number(lng)+0.09009}}]}
-    ]).exec((err, docs) => {
-        if(err){
-            return res.status(400).json({
-                error: true,
-                message: err.message
-            })
-        } else {
-            return res.status(200).json({
-                error: false,
-                message: 'Places within 10km found!',
-                data: docs
-            })
-        }
-    })
-})
+  placeModel
+    .find({ is_deleted: false, is_approved: true })
+    .and([
+      {
+        $and: [
+          { lat: { $gte: Number(lat) - 0.09009 } },
+          { lat: { $lte: Number(lat) + 0.09009 } },
+        ],
+      },
+      {
+        $and: [
+          { lng: { $gte: Number(lng) - 0.09009 } },
+          { lng: { $lte: Number(lng) + 0.09009 } },
+        ],
+      },
+    ])
+    .exec((err, docs) => {
+      if (err) {
+        return res.status(400).json({
+          error: true,
+          message: err.message,
+        });
+      } else {
+        return res.status(200).json({
+          error: false,
+          message: "Places within 10km found!",
+          data: docs,
+        });
+      }
+    });
+});
 
-module.exports = router
+module.exports = router;
--- routes/api/review.js
+++ routes/api/review.js
@@ -1,78 +1,99 @@
-const express = require('express')
-const placeModel = require('../../models/Place')
-const router = express.Router()
+const express = require("express");
+const placeModel = require("../../models/Place");
+const router = express.Router();
 
-const reviewModel = require('../../models/Review')
+const reviewModel = require("../../models/Review");
+const userModel = require("../../models/User");
 
-function getAvgRating(arr){
-    if(arr.length == 0){
-        sum=0
-        arr.forEach((review) => {
-            sum += review.rating
-        })
-        return sum/arr.length
-    } else {
-        return 'No reviews yet'
-    }
-    
+function getAvgRating(arr) {
+  if (arr.length == 0) {
+    sum = 0;
+    arr.forEach((review) => {
+      sum += review.rating;
+    });
+    return sum / arr.length;
+  } else {
+    return "No reviews yet";
+  }
 }
 
 // add a review
-router.post('/addreview', (req, res) => {
-    const {author, place, rating, reviewText} = req.body
-
-    const rev = new reviewModel({author, place, rating, reviewText})
-    rev.save((err, review) => {
-        if(err){
-            return res.status(400).json({
-                error: true,
-                message: err.message
-            })
-        } else {
+router.post("/addreview", (req, res) => {
+  const { authorEmail, place, rating, reviewText } = req.body;
+  userModel.findOne({ email: authorEmail }, (error, user) => {
+    if (error) {
+      return res.status(200).json({
+        error: true,
+        message: error.message,
+      });
+    } else if (user) {
+      const author = user._id;
+      placeModel.findOne(
+        { place_name: place.place_name, lat: place.lat, lng: place.lng },
+        (error, place) => {
+          if (error) {
             return res.status(200).json({
-                error: false,
-                message: 'Review added successfully!',
-                data: review
-            })
+              error: true,
+              message: err.message,
+            });
+          } else if(place) {
+              const placeID = place._id;
+            const rev = new reviewModel({ author, place:placeID , rating, reviewText });
+            rev.save((err, review) => {
+              if (err) {
+                return res.status(200).json({
+                  error: true,
+                  message: err.message,
+                });
+              } else {
+                return res.status(200).json({
+                  error: false,
+                  message: "Review added successfully!",
+                  data: review,
+                });
+              }
+            });
+          }
         }
-    })
-})
+      );
+    }
+  });
+});
 
 // get review by place
-router.get('/getplacereviews', (req, res) => {
-    placeModel.find({place: req.body.place}, (err, docs) => {
-        if(err){
-            return res.status(400).json({
-                error: true,
-                message: err.message
-            })
-        } else {
-            return res.status(200).json({
-                error: false,
-                message: 'Found reviews for place',
-                data: {rating: getAvgRating(docs), reviews: docs}
-            })
-        }
-    })
-})
+router.get("/getplacereviews", (req, res) => {
+  placeModel.find({ place: req.body.place }, (err, docs) => {
+    if (err) {
+      return res.status(400).json({
+        error: true,
+        message: err.message,
+      });
+    } else {
+      return res.status(200).json({
+        error: false,
+        message: "Found reviews for place",
+        data: { rating: getAvgRating(docs), reviews: docs },
+      });
+    }
+  });
+});
 
 // get reviews by author
-router.get('/getauthorreviews', (req, res) => {
-    reviewModel.find({author: req.body.author}, (err, docs) => {
-        if(err){
-            return res.status(400).json({
-                error: true,
-                message: err.message
-            })
-        } else {
-            return res.status(200).json({
-                error: false,
-                message: 'Found reviews for author.',
-                data: docs
-            })
-        }
-    })
-})
+router.get("/getauthorreviews", (req, res) => {
+  reviewModel.find({ author: req.body.author }, (err, docs) => {
+    if (err) {
+      return res.status(400).json({
+        error: true,
+        message: err.message,
+      });
+    } else {
+      return res.status(200).json({
+        error: false,
+        message: "Found reviews for author.",
+        data: docs,
+      });
+    }
+  });
+});
 
-
-module.exports = router
+module.exports = router;
